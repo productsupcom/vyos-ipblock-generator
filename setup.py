4bbf210f59879d4f93b80509c4fd19738a524990
Wieger Bontekoe <wieger.bontekoe@gmail.com>
2025-08-13 16:35:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if os.path.exists("README.md"):
 
 setup(
     name="vyos-ipblock-generator",
-    version="1.0.1",
+    version="1.0.2",
     description="VyOS IP blocklist generator from threat intelligence sources",
     long_description=long_description,
     long_description_content_type="text/markdown",
